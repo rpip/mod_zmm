e162e8a59e885ea571f4723b8cee79379be3f43d
Mawuli Adzaku <mawuli@mawuli.me>
2013-09-24 04:33:49 +0000

    Add header file

---
+++ include/mod_zmm.hrl
@@ -0,0 +1,24 @@
+%% @author Mawuli Adzaku <mawuli@mawuli.me>
+%% @copyright 2013 Mawuli Adzaku
+%% Date: 23-09-2013
+%% @doc Uses JSON data from ZMR as models.
+
+%% Copyright 2013 Mawuli Adzaku
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+
+%% file for caching json data from ZMR 
+-define(ZMM_CACHE, filename:join([z_utils:lib_dir(priv), "modules", ".zmm"])).
+
+-define(zmm_api,"http://modules.zotonic.com/api/zmr/repositories").
